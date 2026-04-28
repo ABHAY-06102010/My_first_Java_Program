709b3b20579ca2caa6517535bf42c2e5f0bc4dd3
ABHAY-06102010 <kamladevisharma564@gmail.com>
2026-04-28 21:45:37 +0530

    Add files via upload

---
+++ Number Programs/AmicableNumbers.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class AmicableNumbers 
+{
+    public static void main(String args[]) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+
+        System.out.print("Enter first number: ");
+        num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        num2 = sc.nextInt();
+
+        int sum1 = 0, sum2 = 0;
+
+        // Sum of proper divisors of num1
+        for(int i = 1; i <= num1/2; i++) 
+        {
+            if(num1 % i == 0) 
+            {
+                sum1 += i;
+            }
+        }
+
+        // Sum of proper divisors of num2
+        for(int i = 1; i <= num2/2; i++) 
+        {
+            if(num2 % i == 0) 
+            {
+                sum2 += i;
+            }
+        }
+
+        // Check if amicable
+        if(sum1 == num2 && sum2 == num1) 
+        {
+            System.out.println(num1 + " and " + num2 + " are amicable numbers.");
+        } else
+        {
+            System.out.println(num1 + " and " + num2 + " are not amicable numbers.");
+        }
+        sc.close();
+    }
+}
